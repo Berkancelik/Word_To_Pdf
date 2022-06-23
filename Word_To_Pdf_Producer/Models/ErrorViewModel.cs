0fc3f2c752269ae6afe12a14ab7687ebdc33540c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-23 16:21:17 +0300

    Add project files.

---
+++ Word_To_Pdf_Producer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Word_To_Pdf_Producer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
