d1e1894666864746a7925c543ac643083ea8a731
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-24 11:17:00 +0300

    Part of converting word file to pdf has been completed, but some errors remain

--- Word_To_Pdf_Producer/Controllers/HomeController.cs
+++ Word_To_Pdf_Producer/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Word_To_Pdf_Producer.Models;
 
@@ -12,10 +17,12 @@ namespace Word_To_Pdf_Producer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
@@ -26,7 +33,50 @@ namespace Word_To_Pdf_Producer.Controllers
         [HttpPost]
         public IActionResult WordToPdfPage(WordToPdf wordToPdf)
         {
-            return View(wordToPdf);
+
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri(configuration["ConnectionStrings:RabbitMQCloudString"]);
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("convert-exchange", ExchangeType.Direct, true, false, null);
+
+                    channel.QueueDeclare("File", true, false, false, null);
+
+                    channel.QueueBind("File", "convert-exchange", "WordToPdf");
+
+                    MessageWordToPdf messageWordToPdf = new MessageWordToPdf();
+
+                    using (MemoryStream ms =new  MemoryStream())
+                    {
+                        wordToPdf.WordFile.CopyTo(ms);
+                        messageWordToPdf.WordByte= ms.ToArray();
+                    }
+
+                    messageWordToPdf.Email = wordToPdf.Email;
+                    messageWordToPdf.FileName = Path.GetFileNameWithoutExtension(wordToPdf.WordFile.FileName);
+
+                    string serializeMessage = JsonConvert.SerializeObject(messageWordToPdf);
+
+                    byte[] ByteMessage = Encoding.UTF8.GetBytes(serializeMessage);
+
+                    var properties = channel.CreateBasicProperties();
+
+                    properties.Persistent = true;
+
+                    channel.BasicPublish("convert-exchange", routingKey: "WordToPdf", basicProperties: properties, body: ByteMessage);
+
+                    ViewBag.result = "Word dosyanız pdf dosyasına dönüştürüldükten sonra size email olarak gönderilecektir.";
+
+                    return View();
+
+                }
+
+            }
+
         }
 
         public IActionResult Privacy()
---
+++ Word_To_Pdf_Producer/Models/MessageWordToPdf.cs
@@ -0,0 +1,9 @@
+﻿namespace Word_To_Pdf_Producer.Models
+{
+    public class MessageWordToPdf
+    {
+        public byte[] WordByte { get; set; }
+        public string Email { get; set; }
+        public string FileName { get; set; }
+    }
+}
